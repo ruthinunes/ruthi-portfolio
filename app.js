f8bc51950110b1c1d8d1628fca7bc32b6fa7a4bf
ruthinunes <pamelanunesdev@gmail.com>
2024-03-22 18:28:40 -0300

    add header shadow

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ const removeMenu = (navMenu) => {
   });
 };
 
+// change background header
+const scrollHeader = () => {
+  const nav = document.getElementById("header");
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition >= 100) {
+    nav.classList.add("scroll-header");
+  } else {
+    nav.classList.remove("scroll-header");
+  }
+};
+
 // remove menu when a nav link is clicked
 const removeMenuOnClick = (navMenu) => {
   const navlinks = document.querySelectorAll(".nav__link");
@@ -38,4 +50,7 @@ const removeClass = (element, className) => {
   element.classList.remove(className);
 };
 
-window.addEventListener("DOMContentLoaded", displayMenu);
+window.addEventListener("DOMContentLoaded", () => {
+  scrollHeader();
+  displayMenu();
+});
