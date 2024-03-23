e03a60b9a806406d2e36344053370713e3ecee5c
ruthinunes <pamelanunesdev@gmail.com>
2024-03-23 14:18:57 -0300

    Create function to remove menu when a nav link is clicked

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// *** NAV
 // open menu mobile
 const displayMenu = () => {
   const navMenu = document.getElementById("nav-menu");
@@ -19,19 +20,7 @@ const removeMenu = (navMenu) => {
   });
 };
 
-// change background header
-const scrollHeader = () => {
-  const nav = document.getElementById("header");
-  const scrollPosition = window.scrollY;
-
-  if (scrollPosition >= 100) {
-    nav.classList.add("scroll-header");
-  } else {
-    nav.classList.remove("scroll-header");
-  }
-};
-
-// remove menu when a nav link is clicked
+// remove menu mobile when a nav link is clicked
 const removeMenuOnClick = (navMenu) => {
   const navlinks = document.querySelectorAll(".nav__link");
 
@@ -42,12 +31,58 @@ const removeMenuOnClick = (navMenu) => {
   });
 };
 
+// active nav section link when scroll
+const activeNavLink = () => {
+  const sections = document.querySelectorAll("section[id]");
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    updateSections(current, scrollY);
+  });
+};
+
+// add menu shadow when scroll
+const setHeaderShadow = () => {
+  const nav = document.getElementById("header");
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition >= 100) {
+    nav.classList.add("scroll-header");
+  } else {
+    nav.classList.remove("scroll-header");
+  }
+};
+
+const updateSections = (current, scrollY) => {
+  const sectionId = current.getAttribute("id");
+  const isActive = isSectionActive(current, scrollY);
+  toggleActiveClass(sectionId, isActive);
+};
+
+const isSectionActive = (current, scrollY) => {
+  const sectionHeight = current.offsetHeight;
+  const sectionTop = current.offsetTop - 50;
+  return scrollY > sectionTop && scrollY <= sectionTop + sectionHeight;
+};
+
+const toggleActiveClass = (sectionId, isActive) => {
+  const link = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+  if (isActive) {
+    addClass(link, "active-link");
+  } else {
+    removeClass(link, "active-link");
+  }
+};
+
+// *** FOOTER
+// automatically update footer year
 const setFooterYear = () => {
   const year = new Date().getFullYear();
   document.getElementById("footer-year").innerHTML = `&#169 ${year}`;
 };
 
-// reusable functions
+// *** reusable functions
 const addClass = (element, className) => {
   element.classList.add(className);
 };
@@ -61,4 +96,7 @@ window.addEventListener("DOMContentLoaded", () => {
   displayMenu();
 });
 
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener("scroll", () => {
+  activeNavLink();
+  setHeaderShadow();
+});
