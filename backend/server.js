d559a38fde691675ac750ac5310a9ebaadc262e2
ruthinunes <pamelanunesdev@gmail.com>
2024-06-04 15:32:11 -0300

    fix: Configure CORS to allow requests from specific origin
    - Updated server.js to configure CORS and allow requests from https://ruthinunes.com

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://ruthinunes.com" }));
 app.use("/api", routerUrls);
 
 dotenv.config();
